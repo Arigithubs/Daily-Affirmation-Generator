d5125f1ce29384b4c3a6dd65413ed79491e0da29
Arigithubs <arimail@duck.com>
2023-12-26 02:50:24 -0500

    Update affirmations with more empowering language

--- script.js
+++ script.js
@@ -3,28 +3,51 @@ document.addEventListener('DOMContentLoaded', function () {
     const generateAffirmationButton = document.getElementById('generateAffirmation');
 
     const affirmations = [
-        "Today, I am filled with gratitude.",
-        "I attract positivity and abundance into my life.",
-        "I am confident and capable of achieving my goals.",
-        "My potential is limitless.",
-        "I choose to let go of negative thoughts and embrace positivity.",
-        "I am worthy of love and happiness.",
-        "I trust in the process of life.",
-        "I am at peace with who I am.",
-        "I radiate confidence, self-respect, and inner harmony.",
-        "I am a beacon of love and compassion."
+        "Embracing the beauty of this moment, I am filled with gratitude.",
+        "As I navigate my day, I effortlessly attract positivity and abundance.",
+        "Confident in my abilities, I gracefully achieve my goals.",
+        "In the vast expanse of my potential, I discover limitless possibilities.",
+        "Releasing negativity, I choose to embrace positivity in every thought.",
+        "Worthy of love and happiness, I radiate these feelings to the world.",
+        "Trusting the intricate process of life, I find peace in each step.",
+        "Harmonizing with my true self, I am at peace with who I am.",
+        "Radiating confidence, self-respect, and inner harmony, I am unstoppable.",
+        "A beacon of love and compassion, I create a ripple of positive impact."
     ];
 
     function generateRandomAffirmation() {
         generateAffirmationButton.disabled = true;
         const randomIndex = Math.floor(Math.random() * affirmations.length);
-        affirmationText.textContent = affirmations[randomIndex];
-        setTimeout(function () {
-            generateAffirmationButton.disabled = false;
-        }, 1000);
+        const chosenAffirmation = affirmations[randomIndex];
+
+        animateText(chosenAffirmation, affirmationText, 100, 30, function () {
+            setTimeout(function () {
+                generateAffirmationButton.disabled = false;
+            }, 1000);
+        });
     }
 
     generateAffirmationButton.addEventListener('click', generateRandomAffirmation);
 
     generateRandomAffirmation();
+
+    // Function to animate text
+    function animateText(text, element, speed, delay, callback) {
+        const characters = text.split('');
+        let currentIndex = 0;
+
+        function displayNextCharacter() {
+            if (currentIndex < characters.length) {
+                element.innerHTML += characters[currentIndex];
+                currentIndex++;
+                setTimeout(displayNextCharacter, speed);
+            } else {
+                if (callback) {
+                    setTimeout(callback, delay);
+                }
+            }
+        }
+
+        displayNextCharacter();
+    }
 });
