04355fe07e3d53f1aa8c01d00376db4f0ccd65b0
Arigithubs <arimail@duck.com>
2023-12-31 13:15:13 -0500

    Refactor affirmations and add new ones

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const affirmationText = document.getElementById('affirmationText');
     const generateButton = document.getElementById('generateAffirmation');
-
+    
     const affirmations = [
         "Embracing the beauty of this moment, I am filled with gratitude.",
         "As I navigate my day, I effortlessly attract positivity and abundance.",
@@ -13,42 +13,54 @@ document.addEventListener('DOMContentLoaded', () => {
         "Harmonizing with my true self, I am at peace with who I am.",
         "Radiating confidence, self-respect, and inner harmony, I am unstoppable.",
         "A beacon of love and compassion, I create a ripple of positive impact.",
-        "Every day, I am becoming a more enlightened version of myself.",
-        "I am a sanctuary of calm, unwavering in the face of chaos.",
-        "With every breath, I inhale confidence and exhale doubt.",
-        "I am the architect of my life; I build its foundation and choose its contents.",
-        "Today, I am brimming with energy and overflowing with joy.",
-        "My body is healthy; my mind is brilliant; my soul is tranquil.",
-        "I am superior to negative thoughts and low actions.",
-        "I have been given endless talents which I begin to utilize today.",
-        "I forgive those who have harmed me in my past and peacefully detach from them.",
-        "A river of compassion washes away my anger and replaces it with love.",
-        "I am guided in my every step by Spirit who leads me towards what I must know and do.",
-        "(For business owners) My business is growing, expanding, and thriving.",
-        "Creative energy surges through me and leads me to new and brilliant ideas.",
-        "Happiness is a choice. I base my happiness on my own accomplishments and the blessings I've been given.",
-        "My ability to conquer my challenges is limitless; my potential to succeed is infinite."
-        // ... Add more unique affirmations as needed
+        "I am surrounded by abundance and prosperity.",
+        "Every challenge I face is an opportunity to grow and improve.",
+        "I am focused, persistent, and will never quit.",
+        "I am worthy of success and all my dreams are coming true.",
+        "I am full of positive loving energy.",
+        "I welcome new experiences and open doors to new opportunities.",
+        "I am in harmony with the beauty of the universe.",
+        "My life is full of happiness, peace, and love.",
+        "I am grateful for my healthy body, my calm mind, and my vibrant soul.",
+        "I am a magnet for creative ideas and solutions.",
+        "I choose to see the good in the people I interact with today.",
+        "I trust myself and my instincts.",
+        "Every day I am getting closer to achieving my goals.",
+        "I am an embodiment of peace and tranquility.",
+        "I celebrate my individuality and use my unique talents positively."
     ];
-    
 
-    const createColorPalette = () => {
-        const hue = Math.floor(Math.random() * 360);
-        const pastel = `hsl(${hue}, 100%, 87.5%)`;
-        document.body.style.backgroundColor = pastel;
+    // Function to dynamically create a color palette for background
+    const createDynamicPalette = () => {
+        const hue = Math.random() * 360;
+        document.body.style.backgroundColor = `hsl(${hue}, 75%, 85%)`;
     };
 
-    const typeWriterEffect = (text, index = 0) => {
-        if (index < text.length) {
-            affirmationText.textContent = text.substring(0, index + 1);
-            setTimeout(() => typeWriterEffect(text, index + 1), 50); // Recursive call for the next character
-        }
+    // Typewriter effect for displaying affirmations
+    const typeWriterEffect = (text, element) => {
+        let index = 0;
+        element.textContent = ''; // Clear the text
+
+        return new Promise((resolve) => {
+            const timer = setInterval(() => {
+                if (index < text.length) {
+                    element.textContent += text.charAt(index);
+                    index++;
+                } else {
+                    clearInterval(timer);
+                    resolve(); // Resolve the promise after the animation
+                }
+            }, 50); // Typing speed in milliseconds
+        });
     };
 
-    const generateRandomAffirmation = () => {
+    // Generate and display a random affirmation
+    const generateRandomAffirmation = async () => {
         const randomIndex = Math.floor(Math.random() * affirmations.length);
-        typeWriterEffect(affirmations[randomIndex]);
-        createColorPalette(); // Change background color with new affirmation
+        const selectedAffirmation = affirmations[randomIndex];
+
+        await typeWriterEffect(selectedAffirmation, affirmationText);
+        createDynamicPalette(); // Change the background color with new affirmation
     };
 
     generateButton.addEventListener('click', generateRandomAffirmation);
