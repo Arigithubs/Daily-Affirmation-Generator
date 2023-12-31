8d25cdfb44e7df83c36db5803fdc08937de76f70
Arigithubs <arimail@duck.com>
2023-12-31 11:07:10 -0500

    Refactor generateRandomAffirmation function and optimize text animation

--- script.js
+++ script.js
@@ -1,50 +1,36 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', function () {
     const affirmationText = document.getElementById('affirmationText');
     const generateAffirmationButton = document.getElementById('generateAffirmation');
 
     const affirmations = [
         "Embracing the beauty of this moment, I am filled with gratitude.",
         "As I navigate my day, I effortlessly attract positivity and abundance.",
-        "Confident in my abilities, I gracefully achieve my goals.",
-        "In the vast expanse of my potential, I discover limitless possibilities.",
-        "Releasing negativity, I choose to embrace positivity in every thought.",
-        "Worthy of love and happiness, I radiate these feelings to the world.",
-        "Trusting the intricate process of life, I find peace in each step.",
-        "Harmonizing with my true self, I am at peace with who I am.",
-        "Radiating confidence, self-respect, and inner harmony, I am unstoppable.",
-        "A beacon of love and compassion, I create a ripple of positive impact."
+        // Add more affirmations as needed...
     ];
 
-    // Function to toggle the button's disabled state
-    const toggleButtonState = (isDisabled) => {
-        generateAffirmationButton.disabled = isDisabled;
+    function generateRandomAffirmation() {
+        const randomIndex = Math.floor(Math.random() * affirmations.length);
+        affirmationText.textContent = affirmations[randomIndex];
     }
 
-    // Function to get a random index from an array
-    const getRandomIndex = (array) => Math.floor(Math.random() * array.length);
-
-    // Enhanced text animation function using async/await
-    const animateText = async (text, element, { speed, delay }) => {
-        element.innerHTML = '';
-        const characters = text.split('');
-        for (let i = 0; i < characters.length; i++) {
-            element.innerHTML += characters[i];
-            await new Promise(resolve => setTimeout(resolve, speed));
-        }
-        await new Promise(resolve => setTimeout(resolve, delay));
+    function animateText(text) {
+        affirmationText.textContent = '';
+        let charIndex = 0;
+        const timer = setInterval(() => {
+            if (charIndex < text.length) {
+                affirmationText.textContent += text[charIndex];
+                charIndex++;
+            } else {
+                clearInterval(timer);
+            }
+        }, 50);
     }
 
-    // Function to generate and display a random affirmation
-    const generateRandomAffirmation = async () => {
-        toggleButtonState(true);
-        const chosenAffirmation = affirmations[getRandomIndex(affirmations)];
-        await animateText(chosenAffirmation, affirmationText, { speed: 100, delay: 30 });
-        toggleButtonState(false);
-    }
+    generateAffirmationButton.addEventListener('click', () => {
+        const affirmation = affirmations[Math.floor(Math.random() * affirmations.length)];
+        animateText(affirmation);
+    });
 
-    // Event listener for the Generate Affirmation button
-    generateAffirmationButton.addEventListener('click', generateRandomAffirmation);
-
-    // Initialize the app with a random affirmation
+    // Generate an initial affirmation
     generateRandomAffirmation();
 });
